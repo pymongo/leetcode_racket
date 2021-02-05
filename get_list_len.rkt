9df56a5e800aa841b97183d656ad50dc740f4c41
wuaoxiang <pylint@yandex.com>
2021-02-05 17:08:19 +0800

    add racket list len function

---
+++ get_list_len.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(define a '(1, 1+4i, #f))
+(list? a)
+; car get first of list, cadr/caddr get 2th/3th of list
+(cadr a)
+; cdr get list exclude first
+(cdr a)
+
+(define (len _list)
+  (if (empty? _list) 0
+      (+ 1 (len (cdr _list)))
+      )
+  )
+(require racket/trace)
+(trace len)
+(len '(1,2,3))
